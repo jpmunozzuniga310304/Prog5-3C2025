ee228c589551de06618eb5ddd852a8f2b3f95458
José Pablo Muñoz Zúñiga <josepablo.mz04@gmail.com>
2025-10-15 04:32:02 -0600

    P-06 se agrego Centro de Vacunación, el cual al agregar algo, se pone en la Base de Datos de SSMS, cuya Base de Datos se crea por medio del Visual Studio por medio de update-database

---
+++ Prog5-3C2025/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prog5_3C2025.Models;
+
+namespace Prog5_3C2025.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<EstudianteUH> EstudianteUH { get; set; } = default!;
+        public DbSet<CentroVacunacion> CentroVacunacion { get; set; } = default!;
+    }
+}
---
+++ Prog5-3C2025/Migrations/20251015091224_CentroVacunacion.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prog5_3C2025.Migrations
+{
+    /// <inheritdoc />
+    public partial class CentroVacunacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CentroVacunacion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProvinciaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CentroVacunacion", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstudianteUH",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstudianteUH", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CentroVacunacion");
+
+            migrationBuilder.DropTable(
+                name: "EstudianteUH");
+        }
+    }
+}
